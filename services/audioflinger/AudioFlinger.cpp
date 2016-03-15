216feed6c93c4a20319bdb0939371be75364d45f
Dmitry Smirnov <divis1969@gmail.com>
2016-03-15 23:15:13 +0300

    Disable check for stream frequency
    This is a temprorary solution for a microphone.
    Audio policy with stereo channels only still tries to
    open a stream for mono which is rejected by MTK HAL
    
    Change-Id: I11ab16abf0cd0b8d4bb1f49bceb4aa5ae25e6c52

--- services/audioflinger/AudioFlinger.cpp
+++ services/audioflinger/AudioFlinger.cpp
@@ -2134,7 +2134,7 @@ sp<AudioFlinger::RecordThread> AudioFlinger::openInput_l(audio_module_handle_t m
     if (status == BAD_VALUE &&
         audio_is_linear_pcm(config->format) &&
         audio_is_linear_pcm(halconfig.format) &&
-        (halconfig.sample_rate >= AUDIO_RESAMPLER_DOWN_RATIO_MAX * config->sample_rate) &&
+        /* (halconfig.sample_rate >= 2 * config->sample_rate) && */
         (audio_channel_count_from_in_mask(halconfig.channel_mask) <= FCC_2) &&
         (audio_channel_count_from_in_mask(config->channel_mask) <= FCC_2)) {
         // FIXME describe the change proposed by HAL (save old values so we can log them here)
